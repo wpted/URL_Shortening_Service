48520e9f346639bd3623e47283991880c4ec7f21
wpted <silverhandsome@gmail.com>
2023-11-15 21:33:48 +0800

    Feat: Create universal errors for package repository.

--- KeyGenerationService/internal/repository/database.go
+++ KeyGenerationService/internal/repository/database.go
@@ -1,8 +1,15 @@
 package repository
 
+import "errors"
+
 // KGSDatabase is the interface that wraps writing and fetching keys from a Key Generation Service Database.
 type KGSDatabase interface {
 	KeyExist(string) (bool, error)
 	WriteKey(string) error
 	GetKeys(int) ([]string, error)
 }
+
+var (
+	ErrKeyNotFound = errors.New("error desired key isn't found in database")
+	ErrNegativeKey = errors.New("error cannot have 0 or negative requiredKeys")
+)
